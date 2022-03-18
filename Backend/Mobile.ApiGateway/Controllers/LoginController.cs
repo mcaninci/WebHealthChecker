62fb4f5585e88e5631456029070901afa3946d87
Mehmet Can İnci <64218936+mcaninci@users.noreply.github.com>
2022-03-18 17:39:27 +0300

    clean comment

--- Backend/Mobile.ApiGateway/Controllers/LoginController.cs
+++ Backend/Mobile.ApiGateway/Controllers/LoginController.cs
@@ -174,7 +174,8 @@ namespace Mobile.ApiGateway.Controllers
             GenericResponse<int?> saveResponse = ManuelDbOperation.InsertUserDefinition(userDefinitionModel);
             if (!saveResponse.IsSuccess || saveResponse.Value == null)
             {
-                return BadRequest(userDefinitionModel);
+            
+                return BadRequest(saveResponse);
             }
 
 
@@ -200,89 +201,6 @@ namespace Mobile.ApiGateway.Controllers
             }
             return (hashCode);
         }
-
-
-        // private string GenerateUserCode()
-        // {
-        //     Random r = new Random();
-        //     string userCode = "";
-        //     for (int i = 0; i < 8; i++)
-        //     {
-        //         int a = r.Next(2);
-        //         int character;
-        //         switch (a)
-        //         {
-        //             case 0: // rakam 
-        //                 character = r.Next(0, 9);
-        //                 userCode = userCode + character.ToString();
-        //                 break;
-        //             case 1: // buyuk harf
-        //                 character = r.Next(65, 90);
-        //                 userCode = userCode + Convert.ToChar(character).ToString();
-        //                 break;
-        //             //case 2: // kucuk harf
-        //             //    character = r.Next(97, 122);
-        //             //    s = s + Convert.ToChar(character).ToString();
-        //             //    break;
-        //         }
-        //     }
-
-        //     UserDefinition checkUserCode = _userDefinitionRepository.GetList<UserDefinition>(new { Code = userCode }).FirstOrDefault();
-        //     if (checkUserCode != null)
-        //     {
-        //         return GenerateUserCode();
-        //     }
-
-        //     return userCode;
-        // }
-        //
-        //todo : sign in service
-        //sign -up
-        //get servisi hash code a göre 
-        //user id ye göre get
-        //billing hash code + user id ile olacak
-
-        //     [HttpPost]
-        //     //[HttpPost("~/connect/token"), Produces("application/json")]
-        //     [BasicAuthorization]
-        //     public ActionResult<GenericResponse<UserDefinition>> GetUserByUserId([FromBody] int userId)
-        //     {
-        //         //return LoginController.DummyGetUserByUserId(userId);
-
-        //   GenericResponse<UserDefinition> returnObject = new GenericResponse<UserDefinition>();
-        //         var userdata= _userReferenceRepository.Get<UserDefinition>(userId);
-        //         returnObject.Value=userdata;
-        //      return new ActionResult<GenericResponse<UserDefinition>>(returnObject);
-
-        //     }
-
-
-
-        //        private static ActionResult<UserDefinition> DummyGetUserByUserId(int userId)
-        // {
-        //     UserDefinition userDefinition = new UserDefinition();
-        //     userDefinition.Id = userId;
-        //     userDefinition.InsertDate = DateTime.Now;
-        //     userDefinition.Name = "Sami";
-        //     userDefinition.Surname = "Dulger";
-        //     userDefinition.Instagram = "samidulger@instagram.com";
-        //     userDefinition.LinkedIn = "samidulger@linkedin.com";
-        //     userDefinition.Mail = "sami_dulger@hotmail.com";
-        //     userDefinition.ReferenceCode = "Sd3738";
-        //     userDefinition.IsVerify = true;
-        //     userDefinition.HashCode = "123456";
-        //     userDefinition.Password = "12345sd";
-        //     userDefinition.Twitter = "https://twitter.com/samidulger";
-        //     userDefinition.UserType = UserType.Diamond;
-        //     userDefinition.Image =
-        //         "iVBORw0KGgoAAAANSUhEUgAAAQAAAAEACAMAAABrrFhUAAAAA3NCSVQICAjb4U/gAAAACXBIWXMAAAhDAAAIQwGFAW1/AAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAAAm1QTFRF////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAATkofWQAAAM50Uk5TAAECAwQFBgcICQoLDA0ODxAREhMUFRcYGRobHB0eHyAhIiMlJygpKi4vMDEyMzQ1Njc5Ojs8Pj9AQkNISUpLTE1OT1BSVVZXWFlbXF1eX2BhYmNlZmdoamxtbm9wcXJzdXZ3eHl6e3x9fn+AgYOFhoeIiYqLjZCRlJWWl5iZmpufoaKkpaanqKmqq6ytrq+ztLa3uLm7vL2+v8HDxcfIycrLzc/Q0dLU1dbX2dvd3t/g4eLk5ebn6Onq6+zt7u/w8fLz9PX29/j5+vv8/f4eL1yDAAAG1ElEQVQYGeXBiXeU1RkH4N/MJJPZkiEgCSiNQrQiKhabBApBo41UbKlbkeISC0VtqrKY2NJYFSzFiti6DNEacxNcEMsiVk1MWAx2CCST+f1N7enxnH5URJbc93vv3OcBlChr2br/0Fz46vsdA/yPm+Cl7C8M/2u4Av6JLP7TCX7tKXjn8scP8X/mwC+pn75VZMAgvHLjcyM8XQ7+mLHmH/yGHfBEeevfCjyDHnhh7u+GeWb56Sh5U3/5Lr/d71HiGraf5Fl1laF0Vfz8XX6nt6aiRM188guei3+uqUEJunH7GM/V2M7mKEpK/Ge7eX6G/nz391AqZjwxyAux7w8/zsJ9C7aN8YIVep9cGIfLGrt5sf71ym1JOKqxm5Pi+AvNZXBPQzcnz3BHNdxSs42T62hbHO6IrjrGSXfwJxE44vJ+WtFXDydcN0BLDjfAAUu+ojWjy6De3eO0qPgrKPdbWvZMFJq10bonoFjjOK0rLoFaNZ9TwJE6KBX7O0X0x6HTJgp5BirNL1LKD6DRXyhmBxSaXaCYiTnQZzMFbYY60/IUlJ8Gbdopqh3a7KOovVAmU6SoQhK6/JDCFkCXVRS2Ero8R2F/hC79FNYHVWKjFDYagyZZistCkyzFZaFJFcVloUkVxWWhSRXFZaFJJcVVQZNKiquCJhmKq4ImGYqrgiYZiquCJmmKq4QmaYqrhCYpiquEJimKq4QmKYqrhCZJistAkyTFZaBJkuIy0CRBcRlokqC4DDSpoLg0NKmguDQ0qaC4NDSJU1wamsQpLg1NyikuBU3KKS4FTcopLgVNyiguBU3KKC4FTWIUl4QmMYpLQpMYxSWhSZTiktAkSnFJaBKhuAQ0iVBcAppEKC4BVSguAVUoLgFNplPcTGiyhOKaoUknxT0ORZYUKO7Uj6DFJZvHGYLxzZdAhdhuhmR3DBrcztDcDg3WMzTroUEnQ9MJDR5maB6GBrcwNLdAg1kMzSyocJghOQwddjEku6BDB0PSAR2WMyTLocNVDMlV0CGaZyjyUSjRy1D0QosuhqILWtzLUNwLLeYzFPOhRWKcIRhPQI09DMEe6LGVIdgKPR5kCB6EHosYgkXQYwpDMAWKHKS4g9BkJ8XthCaPUdxj0GQhxS2EJtFPKezTKFTppLBO6DKPwuZBmb0UtRfaPEJRj0CbqynqaqhziIIOQZ8uCuqCPi0U1AJ90icp5mQaCnVTTDc02kAxG6BRK8W0QqM6iqmDSsMUMgyd3qCQN6DTWgpZC52uoJAroFQfRfRBqzaKaINWs4oUUJwFtbopoBt6NVFAExTrpnXd0KyB1jVAtRwty0G3a07RqlPXQLk2WtUG7SI5WpSLQL3aIVozVAsH3ExrboYTXqYlL8MN9bSkHo4YoRUjcMUHtOIDuOIArTgAV+RpRR6OqKQllXBDPS2phxuaaEkT3LCMliyDG+6nJffDDRtoyQa4YQst2QI3vE5LXocb3qMl78ENg7RkEE6ITdCSiRhcUEtrauGCebRmHlzQTGua4YK7aM1dcMFGWrMRDogP0JqBOPTbSIuegna122nVjplQ7dd5WnZiHRRbTQGrodbsAgUUZkOrTorohFLJIxRxJAmd7qGQe6BTP4X0Q6U6iqmDRg9RzEPQqIdieqBQzQTFTNRAn5UUtBL65CgoB3WqxyhorBrarKCoFVBmytsU9fYUaJJcc5TCjq5JQouy+z5jCD67rwwaRJbvZ0j2L48gdEvfZ4jeX4pwNfYwZD2NCM/cV6nAq3MRjjnbilShuG0O5M3oGqMaY10zIKt6U56q5DdVQ05q3TGqc2xdCjLKVw1QpYFV5bAvescBqnXgjigsa9lD1fa0wKamd6jeO02w5drX6ITXroUN9S8W6Yjii/WYbJc+O06HjD97KSbT1I4TdMyJjqmYLOlHv6SDvnw0jckQXz1IRw2ujuNiRVd8TId9vCKKi3Lrh3Tch7fiwi3qZQnoXYQLc32OJSJ3Pc7flS8VWTKKL12J83PZ8wWWlMLzl+HcTXt6lCVn9OlpODeZ9hGWpJH2DL5bxQNDLFlDD1Tg7GJ3fsKS9smdMZxF60cseR+14tss7qMX+hbjTG7YRW/sugHfsJ5eWY/TRbroma4IghbQOwsQ1E7vtCPoTXrnTQQZescgyNA7BkGG3jEIMvSOQZChdwyCDL1jEGToHYMgQ+8YBBl6xyDI0DsGQYbeMQgy9I5BkKF3DIIMvWMQZOgdgyBD7xgEGXrHIMjQOwZBht4xCDL0jkGQoXcMggy9YxBk6B2DoJ30zl8RtJbe+Q2Cpg/RM4drcZqlx+mV4zfh/9Rt2VekJ4r7ttTha/8GDrmYl1PRgW4AAAAASUVORK5CYII=";
-        //     userDefinition.Profit = (decimal)500.15;
-        //     return new ActionResult<UserDefinition>(userDefinition);
-        // }
-
-
-
-
 
 
     }
